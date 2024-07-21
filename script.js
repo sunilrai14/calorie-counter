319fe9ba865da9f2c0f0937f6a8e5d194d5d2cb6
Sunil Rai <mail4sunilr@gmail.com>
2024-07-21 16:19:39 +0530

    Second Commit

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex,'');
+}
 
+function isInvalidInput() {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber= document.querySelectorAll('input[type="text"]').length;
+}
