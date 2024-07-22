e1ff059fc7cd982aacf095e643474d96fefcedf9
Sunil Rai <mail4sunilr@gmail.com>
2024-07-22 12:08:45 +0530

    Third Commit

--- script.js
+++ script.js
@@ -18,5 +18,21 @@ function isInvalidInput() {
 
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber= document.querySelectorAll('input[type="text"]').length;
+    const entryNumber= document.querySelectorAll('input[type="text"]').length+1;
+    const HTMLString = `
+        <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+        <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name">   
+        <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label> 
+        <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories">  
+    `;
+    targetInputContainer.insertAdjacentHTML("beforeend",HTMLString);
+}
+
+addEntryButton.addEventListener("click",addEntry);
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+    for(const item of list) {
+        
+    }
 }
